1bc8fecf9f5d494c4b33d8827937c5851dac7a35
gunha <wh8299@gmail.com>
2024-10-07 15:47:47 +0900

    10/07 error,priorityQueue

---
+++ typescript/build/structure/error.js
@@ -0,0 +1,32 @@
+"use strict";
+const error = () => {
+    const err = new Error(`error message`);
+    err.name = `TypeError`;
+    console.log(`발생하는 에러 : `, err.stack);
+};
+class ArrayBufferOverflow extends Error {
+    constructor(message) {
+        super(message);
+        this.name = `ArrayBufferOverFlow`;
+        this.date = new Date();
+    }
+}
+const test = (length, iterate, item) => {
+    try {
+        console.log("test");
+        let array = new Array(length);
+        for (let i = 0; i < iterate; i++) {
+            console.log("test2");
+            if (i + 1 > length) {
+                throw new ArrayBufferOverflow("Buffer has maximum size");
+            }
+            array.push(item);
+        }
+    }
+    catch (error) {
+        if (error instanceof ArrayBufferOverflow) {
+            console.error(`${error.name}: ${error.message}, date : ${error.date}`);
+        }
+    }
+};
+test(5, 6, 1);
--- typescript/build/structure/priorityQueue.js
+++ typescript/build/structure/priorityQueue.js
@@ -1,18 +1,18 @@
 "use strict";
-class priorityQueueHeap {
-    constructor(numhHeap) {
-        this.heap = new Array(numhHeap);
-        this.numhHeap = 0;
+class priorityQueueMinHeap {
+    constructor(numHeap) {
+        this.heap = new Array(numHeap);
+        this.numHeap = 0;
     }
     insert(newItem) {
         try {
-            if (this.numhHeap >= this.heap.length) {
+            if (this.numHeap >= this.heap.length) {
                 throw new Error("HeapErr: Overflow!");
             }
-            this.heap[this.numhHeap] = newItem;
-            this.percolateUp(newItem, this.numhHeap);
-            this.numhHeap++;
-            // **ts error객체 사용법 알아보기**
+            this.heap[this.numHeap] = newItem;
+            this.percolateUp(newItem, this.numHeap);
+            this.numHeap++;
+            // **error객체 사용법 알아보기**
         }
         catch (error) {
             if (error instanceof Error) {
@@ -33,18 +33,59 @@ class priorityQueueHeap {
     print() {
         return (console.log(this.heap.join("-")));
     }
+    deleteMin() {
+        //왼쪽 자식의 인덱스 : (부모의 인덱스) * 2 + 1
+        //오른쪽 자식의 인덱스 : (부모의 인덱스) * 2 + 2
+        let min = this.heap[0];
+        this.heap[0] = this.heap[this.numHeap - 1];
+        this.heap[this.numHeap - 1] = undefined;
+        this.numHeap--;
+        this.percolateDown(0);
+        return console.log(`삭제된 최소 값 : ${min}`);
+    }
+    percolateDown(parentIndex) {
+        let leftChild = parentIndex * 2 + 1;
+        let rightChild = parentIndex * 2 + 2;
+        if (leftChild >= this.numHeap - 1)
+            return;
+        let minChild;
+        if (rightChild >= this.numHeap - 1) {
+            minChild = leftChild;
+        }
+        else {
+            minChild = this.heap[leftChild] > this.heap[rightChild] ? rightChild : leftChild;
+        }
+        if (this.heap[minChild] < this.heap[parentIndex]) {
+            let temp = this.heap[parentIndex];
+            this.heap[parentIndex] = this.heap[minChild];
+            this.heap[minChild] = temp;
+            this.percolateDown(minChild);
+        }
+    }
+    min() {
+        console.log(`현재의 최소값은 : ${this.heap[0]}`);
+        return this.heap[0];
+    }
+    isEmpty() {
+        console.log(`empty 상태는 : ${this.heap.filter(node => node !== undefined).length > 0}`);
+        return this.heap.filter(node => node !== undefined).length > 0;
+    }
+    clear() {
+        this.heap = new Array(this.numHeap);
+    }
 }
-let heap = new priorityQueueHeap(6);
-// heap.insert(4);
-// heap.insert(1);
-// heap.insert(3);
-// heap.insert(6);
-// heap.insert(5);
-// heap.insert(2);
-// 1-4-2-6-5-3
-heap.insert(3);
-heap.insert(1);
+let heap = new priorityQueueMinHeap(6);
+heap.isEmpty();
 heap.insert(4);
+heap.insert(1);
+heap.insert(3);
+heap.insert(6);
+heap.insert(5);
 heap.insert(2);
+heap.deleteMin();
+heap.min();
+heap.isEmpty();
+heap.print();
+heap.clear();
 heap.print();
 // node priorityQueue.js
