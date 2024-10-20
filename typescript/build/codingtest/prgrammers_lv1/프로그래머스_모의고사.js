b7b35ba2f6a179e1b4050a363e9d9846f0b1fb05
gunha <wh8299@gmail.com>
2024-10-20 22:51:30 +0900

    chore : 폴더 구조 변경

--- typescript/build/codingtest/prgrammers_lv1/프로그래머스_모의고사.js
+++ typescript/build/codingtest/prgrammers_lv1/프로그래머스_모의고사.js
@@ -31,7 +31,7 @@ answers	        return
 모든 사람이 2문제씩을 맞췄습니다.
 
 */
-function solution(answers) {
+function solution_mockexam1(answers) {
     const pattern1 = "12345";
     const pattern2 = "21232425";
     const pattern3 = "3311224455";
@@ -67,5 +67,28 @@ function solution(answers) {
     console.log(answer);
     return answer;
 }
-solution([1, 2, 3, 4, 5]);
-solution([1, 3, 2, 4, 2]);
+function solution_mockexam2(answers) {
+    var answer = [];
+    var a1 = [1, 2, 3, 4, 5];
+    var a2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    var a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    var a1c = answers.filter((a, i) => a === a1[i % a1.length]).length;
+    var a2c = answers.filter((a, i) => a === a2[i % a2.length]).length;
+    var a3c = answers.filter((a, i) => a === a3[i % a3.length]).length;
+    var max = Math.max(a1c, a2c, a3c);
+    if (a1c === max) {
+        answer.push(1);
+    }
+    ;
+    if (a2c === max) {
+        answer.push(2);
+    }
+    ;
+    if (a3c === max) {
+        answer.push(3);
+    }
+    ;
+    return answer;
+}
+// solution([1,2,3,4,5]);
+// solution([1,3,2,4,2]);
